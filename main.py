74eca7d4b3e42ae9d3fead2125177234dc88b057
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-06-25 15:52:03 +0100

    updates

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from Networking.Ethernet import Ethernet
 from Networking.Ipv4 import Ipv4
 from Networking.Icmp import Icmp
 from Networking.Tcp import Tcp
-
+from Networking.Udp import Udp
 
 def main():
     conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -16,22 +16,30 @@ def main():
         raw_data , addr = conn.recvfrom(65565)
 
         ethernet_head = Ethernet(raw_data)
-        ipv4_head     = Ipv4(raw_data)
-        icmp_head     = Icmp(raw_data)
-        tcp_head      = Tcp(raw_data)
 
         print("Ethernet Frame :")
         print(ethernet_head)
 
-        print("IP Header :")
-        print(ipv4_head)
+        if ethernet_head.proto == 8:
+            ipv4_head     = Ipv4(raw_data)
+            print("IP Header :")
+            print(ipv4_head)
 
-        print("ICMP Header :")
-        print(icmp_head)
-
-        print("TCP Header :")
-        print(tcp_head)
-        print(tcp_head.show_flags())
+            if ipv4_head.proto == 1:
+                icmp_head     = Icmp(raw_data)
+                print("ICMP Header :")
+                print(icmp_head)
+            
+            elif ipv4_head.proto == 6:
+                tcp_head      = Tcp(raw_data)
+                print("TCP Header :")
+                print(tcp_head)
+                print(tcp_head.show_flags())
+            
+            elif ipv4_head.proto == 17:
+                udp_head     = Udp(raw_data)
+                print("UDP Header :")
+                print(udp_head)
         
 
 
