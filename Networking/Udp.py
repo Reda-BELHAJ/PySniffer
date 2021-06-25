32056e80e90418e01cb816f0e7b239abd5d9a47c
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-06-25 13:47:11 +0100

    add http udp

---
+++ Networking/Udp.py
@@ -0,0 +1,11 @@
+import struct
+
+
+class UDP:
+
+    def __init__(self, raw_data):
+        self.src_port, self.dest_port, self.size = struct.unpack('! H H 2x H', raw_data[:8])
+        self.data = raw_data[8:]
+    
+    def __str__(self) -> str:
+        pass
